1b92854ae61b3be21bc4d9a42a986454a520c3f7
Vlad Kolesnyk <kolesnikvlad416@gmail.com>
2026-08-20 15:54:53 +0300

    feat: initialize database

---
+++ src/backend/database/database.py
@@ -0,0 +1,17 @@
+import os
+from dotenv import load_dotenv
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
+from sqlalchemy.orm import declarative_base
+
+load_dotenv()
+DATABASE_URL = os.getenv("DATABASE_URL").strip()
+
+engine = create_async_engine(DATABASE_URL, echo=True)
+
+async_session = async_sessionmaker(engine, expire_on_commit=False, class_=AsyncSession)
+
+Base = declarative_base()
+
+async def get_session() -> AsyncSession:
+    async with async_sessionmaker() as session:
+        yield session
