8c713e00178279312ad0199924bc77f1329825d0
Vlad Kolesnyk <kolesnikvlad416@gmail.com>
2026-08-20 20:14:41 +0300

    feat: add rooms schemas

---
+++ src/backend/schemas/rooms_schemas.py
@@ -0,0 +1,37 @@
+from backend.models import Property
+from pydantic import BaseModel, ConfigDict, Field
+from typing import Annotated, List, Optional
+
+
+class RoomBase(BaseModel):
+    name: Annotated(str, Field(min_length=1))
+    price: Annotated(float, Field(default=0.0))
+    capacity: Annotated(int, Field(default=1))
+
+
+class RoomCreate(RoomBase):
+    pass
+
+
+class RoomUpdate(RoomBase):
+    name: Annotated(Optional[str], Field(min_length=1)) = None
+    price: Annotated(Optional[float], Field()) = 0.0
+    capacity: Annotated(Optional[int], Field()) = 1
+    property: Annotated(Optional["PropertyUpdate"], Field())
+    bookings: Annotated(List["BookingUpdate"], Field())
+
+
+class RoomResponse(RoomBase):
+    room_id: int
+    property_id: int
+
+
+class RoomInDB(RoomBase):
+    pass
+
+
+class RoomFullResponse(RoomResponse):
+    model_config = ConfigDict(from_attributes=True)
+
+    property: List["PropertyResponse"] = []
+    bookings: List["BookingResponse"] = []
