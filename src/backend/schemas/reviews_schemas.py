45a34da6a2ec5ab86ac4e8e6855b61bf171ac346
Vlad Kolesnyk <kolesnikvlad416@gmail.com>
2026-08-21 14:02:11 +0300

    style: fix spaces

--- src/backend/schemas/reviews_schemas.py
+++ src/backend/schemas/reviews_schemas.py
@@ -2,17 +2,21 @@ from datetime import datetime
 from pydantic import BaseModel, ConfigDict, Field
 from typing import Annotated, List, Optional
 
+
 class ReviewBase(BaseModel):
     comment: Annotated[Optional[str], Field(default=None)] = None
     rating: Annotated[int, Fielld(ge=1, le=10)]
 
+
 class ReviewCreate(ReviewBase):
     booking_id: int
 
+
 class ReviewUpdate(BaseModel):
     review_id: int
-    rating: Annotated[Optional[int], Field(ge=1, le=10, default=None)]
-    comment: Annotated[Optional[str], Field(default=None)] = None
+    rating: Annotated[Optional[int], Field(ge=1, le=10)] = None
+    comment: Annotated[Optional[str], Field()] = None
+
 
 class ReviewResponse(ReviewBase):
     model_config = ConfigDict(from_attributes=True)
