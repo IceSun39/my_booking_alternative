a8c36056cc579488daa837746fed97a97489f13b
Vlad Kolesnyk <kolesnikvlad416@gmail.com>
2026-08-21 11:33:50 +0300

    feat: add function for login user

--- src/backend/routes/auth.py
+++ src/backend/routes/auth.py
@@ -1,19 +1,25 @@
 from datetime import timedelta
 from fastapi import APIRouter, Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordRequestForm
+from fastapi.responses import JSONResponse
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from src.backend.core.security import create_access_token, decode_token, get_password_hash, verify_password
 from src.backend.services.user_services import UserService
-from src.backend.schemas.users_schemas import  UserResponse, UserCreate
+from src.backend.schemas.users_schemas import UserResponse, UserCreate
 from src.backend.database.database import get_session
+
+from datetime import timedelta
 
 auth_router = APIRouter(
     prefix="/auth",
     tags=["auth"]
 )
 
+REFRESH_TOKEN_EXPIRE_DAYS = 2
+
 UserService = UserService()
+
 
 @auth_router.post("/register", response_model=UserResponse, status_code=status.HTTP_201_CREATED)
 async def register_user(user_data: UserCreate, session: AsyncSession = Depends(get_session)):
@@ -23,3 +29,32 @@ async def register_user(user_data: UserCreate, session: AsyncSession = Depends(g
 
     new_user = UserService.create_user(session, user_data)
     return new_user
+
+
+@auth_router.post("/login", status_code=status.HTTP_200_OK)
+async def login_user(form_data: OAuth2PasswordRequestForm = Depends(),
+                     session: AsyncSession = Depends(get_session)
+                     ):
+    user = await UserService.get_user_by_email(session, form_data.username)
+    if not user or not verify_password(form_data.password, user.password):
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Bearer"}
+        )
+
+    access_token = create_access_token(data={'sub': user.email})
+    refresh_token = create_access_token(data={'sub': user.email}, refresh=True,
+                                        expires_delta=timedelta(days=REFRESH_TOKEN_EXPIRE_DAYS))
+    return JSONResponse(
+        status_code=status.HTTP_200_OK,
+        content={
+            "message": "Login Successful",
+            "access_token": access_token,
+            "refresh_token": refresh_token,
+            "user": {
+                "email": user.email,
+                "user_id": str(user.user_id)
+            }
+        }
+    )
