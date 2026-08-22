7241032ea69c169d3850d9bf6732ddc251bb5568
Vlad Kolesnyk <kolesnikvlad416@gmail.com>
2026-08-22 12:31:03 +0300

    refactor: change dicts responses to JSON responses

--- src/backend/services/favorire_services.py
+++ src/backend/services/favorire_services.py
@@ -1,41 +1,58 @@
+from fastapi import HTTPException
 from typing import Optional, List
 
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
-
+from fastapi.responses import JSONResponse
 
 from src.backend.models.favorites import Favorite
 from src.backend.schemas.favorites_schemas import FavoriteCreate
 
+
 class FavoriteService:
-    async def _get_favorite_in_db(self, session: AsyncSession, property_id: int, user_id, room_id:int) -> Optional[Favorite]:
-        stmt = select(Favorite).where(Favorite.user_id == user_id, Favorite.property_id == property_id, Favorite.room_id == room_id)
+    async def _get_favorite_in_db(self, session: AsyncSession, property_id: int, user_id, room_id: int) -> Optional[
+        Favorite]:
+        stmt = select(Favorite).where(Favorite.user_id == user_id, Favorite.property_id == property_id,
+                                      Favorite.room_id == room_id)
         result = await session.execute(stmt)
         favorite = result.scalar_one_or_none()
         return favorite
 
-    async def get_users_favorites(self,session: AsyncSession, user_id: int) -> Optional[List[Favorite]]:
+    async def get_users_favorites(self, session: AsyncSession, user_id: int) -> Optional[List[Favorite]]:
         stmt = select(Favorite).where(Favorite.user_id == user_id)
         result = await session.execute(stmt)
         return result.scalars().all()
 
-    async def create_favorite(self,session: AsyncSession, user_id: int, favorite_create: FavoriteCreate) -> dict:
-        favorite = await self._get_favorite_in_db(session, user_id, favorite_create.property_id, favorite_create.room_id)
+    async def create_favorite(self, session: AsyncSession, user_id: int,
+                              favorite_create: FavoriteCreate) -> JSONResponse:
+        favorite = await self._get_favorite_in_db(session, user_id, favorite_create.property_id,
+                                                  favorite_create.room_id)
         if favorite:
-            return {"Message": "Favorite already exists", "status": 200}
+            return JSONResponse(
+                content="Favorite already exists",
+                status_code=200,
+            )
 
         favorite = Favorite(user_id=user_id, property_id=favorite_create.property_id)
         session.add(favorite)
         await session.commit()
         await session.refresh(favorite)
-        return {"Message": "Favorite created", "status": 200}
+        return JSONResponse(
+            content="Favorite created",
+            status_code=200
+        )
 
-    async def delete_favorite(self,session: AsyncSession, user_id: int, favorite_id: int, room_id: int) -> dict:
+    async def delete_favorite(self, session: AsyncSession, user_id: int, favorite_id: int, room_id: int) -> JSONResponse:
         favorite = await self._get_favorite_in_db(session, user_id, favorite_id, room_id)
         if favorite is None:
-            return {"Message": "Favorite does not exist", "status": 200}
+            return JSONResponse(
+                content="Favorite does not exist",
+                status_code=200
+            )
 
         await session.delete(favorite)
         await session.commit()
-        return {"Message": "Favorite deleted", "status": 200}
-
+        return JSONResponse(
+            content="Favorite deleted",
+            status_code=200
+        )
